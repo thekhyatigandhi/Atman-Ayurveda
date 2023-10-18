14e96168e28daeb9f1cffe4898ffb8d8062665ba
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-10-18 15:26:19 -0400

    folder structure commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from "react";
+import "./App.css"; // You can style your header in App.css
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Your website content goes here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+// Header.js
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="logo">Your Logo</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Know Your Dosha</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
