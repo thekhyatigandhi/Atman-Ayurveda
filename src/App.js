c7d86b80eeb6691ecac9efb29dec03f530621aa6
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-10-18 15:34:15 -0400

    folder restructuring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-// App.js
-import React from "react";
-import "./App.css"; // You can style your header in App.css
-import Header from "./components/Header";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* Your website content goes here */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
